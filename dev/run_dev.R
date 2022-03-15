ad37913593ef81e2342cea2cbe4bbe9f0adda0f5
AyushBipinPatel <ayush.ap58@gmail.com>
2022-03-15 09:53:47 +0530

    run dev updated as old repo

--- dev/run_dev.R
+++ dev/run_dev.R
@@ -8,5 +8,15 @@ golem::detach_all_attached()
 # Document and reload your package
 golem::document_and_reload()
 
+
+
+# checks before running the app -------------------------------------------
+
+#> in fct_add_intro_method_notes, set year and note number
+
+
+
+
+
 # Run the application
 run_app()
